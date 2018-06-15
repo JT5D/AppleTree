56d1de455ff18bfcb8e24ddb18c19906478e9eb8
JT5D <jamestunick@IMClab2.local>
2018-06-15 02:16:14 -0400

    DesktopCleaner app improvements. Continues to be super handy, used 100s times daily...

---
+++ Applescripts/xcodecleanerb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#change harddrivename &/or rest of path to location of your DerivedData folder
+rm -rf /Users/harddrivename/Library/Developer/Xcode/DerivedData/*
+
+#change path to location of your EmbeddedAppDeltas folder
+rm -rf /private/var/folders/w6/_z6_4xps5q132ghwqpdm6fhm0000gn/C/com.apple.DeveloperTools/All/Xcode/EmbeddedAppDeltas/*
